04292dca96d00bab1659469246e3d916eb6cb932
william molina <molinaramos1235@gmail.com>
2023-10-30 18:03:23 -0500

    subiendo detalles

--- js/download.js
+++ js/download.js
@@ -3,6 +3,28 @@ function download(){
     let email = document.getElementById("ebook-email")
 
     if(name.value != "" && email.value != ""){
-        window.open("../clean-code-free.pdf")
+        var url = 'clean-code-free.pdf';
+        var nombreArchivo = 'clean-code-free.pdf';
+        var link = document.createElement('a');
+        link.download = nombreArchivo;
+        link.href = url;
+        link.click();
+
+        var mensaje = document.getElementsByClassName("mensaje-download")[0]
+        var pantallaNegra = document.getElementsByClassName("pantalla-negra")[0]
+        var contMensaje = document.getElementsByClassName("cont-m-d")[0]
+
+        mensaje.style.top = "25%"
+        pantallaNegra.style.display = "block"
+        contMensaje.style.top = "0"
     }
+}
+
+function cerrarMensaje(){
+    var mensaje = document.getElementsByClassName("mensaje-download")[0]
+    var pantallaNegra = document.getElementsByClassName("pantalla-negra")[0]
+    var contMensaje = document.getElementsByClassName("cont-m-d")[0]
+    pantallaNegra.style.display = "none"
+    mensaje.style.top = "-100%" 
+    contMensaje.style.top = "-100%"
 }
